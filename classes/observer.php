3a21e6cee970146901a9030f2728f7566cb5da71
Luis Perez <luis19prz@gmail.com>
2020-06-24 18:33:19 -0500

    removed log statements

--- classes/observer.php
+++ classes/observer.php
@@ -10,12 +10,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 class local_course_completion_listener_observer {
-    public function __construct() {
-        error_log("in construct");
-        echo "hola desde plugin";
-    }
-
-     
     
     /**
      * Triggered when 'course_module_completion_updated' event is triggered.
@@ -23,13 +17,9 @@ class local_course_completion_listener_observer {
      * @param \core\event\course_module_completion_updated $event
      */
     public static function on_activity_updated(\core\event\course_module_completion_updated $event) {
-        global $DB, $CFG;
-
-        error_log("activity completed from plugin2");
+        global $CFG;
 
         if ($CFG->enablecompletion) {
-            error_log("has enablecompletion4");
-            //error_log( print_r( $event, true ) );
             // Regular Completion cron.
             require_once($CFG->dirroot.'/completion/cron.php');
             /**
